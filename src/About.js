2b9e142118b2f98c9701cc196aa7bb4474182973
Opeyemi Peter <opeyemiadebowale1759@gmail.com>
2021-02-15 22:58:12 -0800

    about icons

--- src/About.js
+++ src/About.js
@@ -1,4 +1,10 @@
 import React from 'react'
+import github from './assests/github.svg';
+import twitter from './assests/twitter.svg';
+import linkedin from './assests/linkedin.svg';
+
+
+
 
 
 function About() {
@@ -8,7 +14,11 @@ function About() {
         <p className="about-session__paragraph">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget lacus tellus. Etiam nec sem nibh. Nullam pulvinar quis sapien at tincidunt. In elit quam, consectetur eget felis bibendum, egestas luctus risus. Etiam ut enim sapien. Ut dictum, mi id malesuada rhoncus, elit velit porttitor purus, sit amet lobortis elit dui eget libero. Praesent nec nibh quis felis faucibus molestie. Nullam vehicula purus quis dui commodo dapibus. Ut sed aliquam enim. Nam porta at nunc quis molestie. Nulla vel justo sit amet erat congue laoreet ut eu tellus.
         </p>
-
+        <div className="about-session__socialLinks">
+          <img src={twitter} alt="twitter" />
+          <img src={github} alt="github" />
+          <img src={linkedin} alt="linkedin" />
+        </div>
     </div>
   )
 }
