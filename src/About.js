50fcf5388cae513d038426ab01441912593ac931
Opeyemi Peter <opeyemiadebowale1759@gmail.com>
2021-02-15 19:36:11 -0800

    setting up

---
+++ src/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function About() {
+  return (
+     <div className="about-session">
+      <h2 className="about-session__heading">About Me</h2>
+      <p className="about-session__paragraph">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti a quisquam ipsa earum maxime illo aut, neque vitae fugit molestiae, provident dolorem. Sunt at ad, ratione provident, non quis possimus itaque enim natus corrupti libero?
+      </p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.scss';
+import About from './About'
+
+function App() {
+  return (
+    <div>
+      <About/>
+    </div>
+  );
+}
+
+export default App;
