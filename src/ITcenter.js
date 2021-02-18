812fdfb894dbfbc00484f7b78ac5d59602fca15d
Opeyemi Peter <opeyemiadebowale1759@gmail.com>
2021-02-17 23:10:49 -0800

    styling the second page

--- src/ITcenter.js
+++ src/ITcenter.js
@@ -6,14 +6,12 @@ import React from 'react'
 function ITcenter() {
   return (
     <div className="It-session">
-      <div className="It-session__img">
         <img src={RectImg} alt="" />
-      </div>
       <div className="It-session__body">
-        <h2 className="It-session__title">
+        <h2 className="It-session__body-title">
           An Introduction into information technology
         </h2>
-        <p className="It-session__paragraph">
+        <p className="It-session__body-paragraph">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget lacus tellus. Etiam nec sem nibh. Nullam pulvinar quis sapien at tincidunt. In elit quam, consectetur eget felis bibendum, egestas luctus risus. Etiam ut enim sapien. Ut dictum, mi id malesuada rhoncus, elit velit porttitor purus, sit amet lobortis elit dui libero. Praesent nec nibh quis felis faucibus molestie. Nullam vehicula purus quis dui commodo dapibus. Ut sed aliquam enim.
         </p>
         <button>START LEARNING</button>
