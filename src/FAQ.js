f644a5c6ca7cf58c22124da9640fa995a7e0452f
Opeyemi Peter <opeyemiadebowale1759@gmail.com>
2021-02-20 22:52:41 -0800

    faq set up

---
+++ src/FAQ.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import plus from './assests/plus.svg'
+
+function FAQ() {
+  return (
+    <div className="faqBody">
+      <h3 className="heading">
+        QUESTIONS FREQUENTLY ASKED  BY OUR VISITORS
+      </h3>
+      <h2 className="title">
+        FAQ
+      </h2>
+      <div>
+        <span className="plus">
+          <img src={plus} alt="" />
+        </span>
+        <p className="text">
+          What stocks and ETFs can I give?
+        </p>
+      </div>
+       <div>
+        <span className="plus">
+          <img src={plus} alt="" /> 
+        </span>
+        <p className="text">
+          What stocks and ETFs can I give?
+        </p>
+      </div>
+
+    </div>
+  )
+}
+
+export default FAQ
+
+What amounts can I give?
+
+
+
+
+
+
+
+How much does it cost?
+
+When does my gift start fluctuating with the market?
+
+What if my recipient wants a different stock instead of the one I gave?
+
+Is Chaka a brokerage?
--- src/ITcenter.js
+++ src/ITcenter.js
@@ -5,16 +5,18 @@ import React from 'react'
 
 function ITcenter() {
   return (
-    <div className="It-session">
-        <img src={RectImg} alt="" />
-      <div className="It-session__body">
-        <h2 className="It-session__body-title">
+    <div className="ItSession">
+        <img className="ItImg" src={RectImg} alt="" />
+      <div className="ItBody">
+        <h2 className="ItTitle">
           An Introduction into information technology
         </h2>
-        <p className="It-session__body-paragraph">
+        <p className="ItParagraph">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eget lacus tellus. Etiam nec sem nibh. Nullam pulvinar quis sapien at tincidunt. In elit quam, consectetur eget felis bibendum, egestas luctus risus. Etiam ut enim sapien. Ut dictum, mi id malesuada rhoncus, elit velit porttitor purus, sit amet lobortis elit dui libero. Praesent nec nibh quis felis faucibus molestie. Nullam vehicula purus quis dui commodo dapibus. Ut sed aliquam enim.
         </p>
-        <button>START LEARNING</button>
+        <a href="#" className="btn">
+          <span class="btn__text">Start Learning</span>
+        </a>
       </div>
     </div>
   )
